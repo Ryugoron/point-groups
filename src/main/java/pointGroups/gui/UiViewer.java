bc9dccc4ff00b5a7ad8815fda9a99b2e5feb3c7a
Marcel Ehrhradt <marehr@zedat.fu-berlin.de>
2013-12-07 15:38:45 +0100

    added zoom support

--- src/main/java/pointGroups/gui/UiViewer.java
+++ src/main/java/pointGroups/gui/UiViewer.java
@@ -9,20 +9,49 @@ import javax.swing.SwingWorker;
 
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
+import de.jreality.plugin.JRViewer.ContentType;
+import de.jreality.plugin.basic.Scene;
+import de.jreality.plugin.content.ContentAppearance;
+import de.jreality.plugin.content.ContentTools;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.Viewer;
+import de.jreality.tools.ClickWheelCameraZoomTool;
 
 
 public class UiViewer
 {
+  public static boolean OLD_INITIALIZE_METHOD = true;
 
   protected static JRViewer initJrViewer() {
     SceneGraphComponent world = new SceneGraphComponent();
-    world.setGeometry(Primitives.sharedIcosahedron);
+    // world.setGeometry(Primitives.sharedIcosahedron);
+    world.setGeometry(Primitives.box(2, 2, 2, true));
+    // world.setGeometry(Primitives.box(0.5, 0.5, 0.5, true));
 
-    JRViewer jrViewer = JRViewer.createJRViewer(world);
-    jrViewer.startupLocal();
-    return jrViewer;
+    JRViewer v;
+
+    if (OLD_INITIALIZE_METHOD) {
+      v = JRViewer.createJRViewer(world);
+      v.registerPlugin(new ContentTools());
+    }
+    else {
+      v = new JRViewer();
+      v.addBasicUI();
+      v.addVRSupport();
+      v.addContentSupport(ContentType.TerrainAligned);
+      v.registerPlugin(new ContentAppearance());
+      v.registerPlugin(new ContentTools());
+      v.setContent(world);
+    }
+
+    {
+      ClickWheelCameraZoomTool zoomTool = new ClickWheelCameraZoomTool();
+      Scene scene = v.getController().getPlugin(Scene.class);
+      scene.getSceneRoot().addTool(zoomTool);
+    }
+
+    v.startupLocal();
+    return v;
   }
 
   public static Future<JRViewer> viewerFactory(final Container component) {
