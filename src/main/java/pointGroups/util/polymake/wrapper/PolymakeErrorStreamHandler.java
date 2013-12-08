127af7f90ced841dd9de24baa7646424bceafaa1
Alex <a.steen@fu-berlin.de>
2013-12-08 18:52:03 +0100

    added log listener for error output of polymake

--- src/main/java/pointGroups/util/polymake/wrapper/PolymakeErrorStreamHandler.java
+++ src/main/java/pointGroups/util/polymake/wrapper/PolymakeErrorStreamHandler.java
@@ -1,17 +1,29 @@
 package pointGroups.util.polymake.wrapper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 class PolymakeErrorStreamHandler implements Runnable {
+	final Logger logger = Logger.getLogger(PolymakeErrorStreamHandler.class.getName());
+	private final BufferedReader err;
 
 	PolymakeErrorStreamHandler(InputStream errorStream) {
-		// TODO Auto-generated constructor stub
+		this.err = new BufferedReader(new InputStreamReader(errorStream));
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		String err;
+		try {
+			while ((err = this.err.readLine()) != null) {
+				logger.severe(err);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
