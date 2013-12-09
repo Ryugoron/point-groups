3b87acf1f70ec970515f3d7a2655654108c1955b
Alexander Steen <a.steen@fu-berlin.de>
2013-12-09 11:35:41 +0100

    Deleted obsolete variables causing a NullPointerException

--- src/main/java/pointGroups/PointGroups.java
+++ src/main/java/pointGroups/PointGroups.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
 import java.util.Collection;
 import java.util.Properties;
 
@@ -14,72 +13,71 @@ import pointGroups.geometry.symmetries.OctahedralSymmetry;
 import pointGroups.util.polymake.SchlegelTransformer;
 import pointGroups.util.polymake.wrapper.PolymakeWrapper;
 
-public class PointGroups {
-	private static final Properties prop = new Properties();
-	private static Socket s;
-	private static Process p;
 
-	public static void main(String[] args) {
+public class PointGroups
+{
+  private static final Properties prop = new Properties();
 
-		/*
-		 * Read configuration
-		 */
-		try {
-			prop.load(new FileInputStream("settings.ini"));
-		} catch (FileNotFoundException e) {
-			System.out.println("Configuration file not found");
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+  public static void main(String[] args) {
 
-		final String polyCmd = prop.getProperty("POLYMAKEPATH");
-		final String polyDriver = prop.getProperty("POLYMAKEDRIVER");
+    /*
+     * Read configuration
+     */
+    try {
+      prop.load(new FileInputStream("settings.ini"));
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("Configuration file not found");
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
 
-		PolymakeWrapper pmWrapper = new PolymakeWrapper(polyCmd, polyDriver);
-		pmWrapper.start();
+    final String polyCmd = prop.getProperty("POLYMAKEPATH");
+    final String polyDriver = prop.getProperty("POLYMAKEDRIVER");
 
-		try {
+    PolymakeWrapper pmWrapper = new PolymakeWrapper(polyCmd, polyDriver);
+    pmWrapper.start();
 
-			// Provisional input
-			System.out.println("Please input a 3D-Point in Format x,y,z:");
-			BufferedReader stdin = new BufferedReader(new InputStreamReader(
-					System.in));
-			String point = stdin.readLine();
-			String[] coords = point.split(",");
+    try {
 
-			Point3D pt = new Point3D(Double.valueOf(coords[0]),
-					Double.valueOf(coords[1]), Double.valueOf(coords[2]));
+      // Provisional input
+      System.out.println("Please input a 3D-Point in Format x,y,z:");
+      BufferedReader stdin =
+          new BufferedReader(new InputStreamReader(System.in));
+      String point = stdin.readLine();
+      String[] coords = point.split(",");
 
-			// Calculate Poins
-			Collection<Point3D> im = OctahedralSymmetry.get().images(pt,
-					OctahedralSymmetry.Subgroups.Full);
+      Point3D pt =
+          new Point3D(Double.valueOf(coords[0]), Double.valueOf(coords[1]),
+              Double.valueOf(coords[2]));
 
-			// Apply Schlegeltransformation script
-			SchlegelTransformer st = new SchlegelTransformer(im);
+      // Calculate Poins
+      Collection<Point3D> im =
+          OctahedralSymmetry.get().images(pt, OctahedralSymmetry.Subgroups.Full);
 
-			pmWrapper.sendRequest(st);
-			// Since Transformer is not yet a future
-			Thread.sleep(2000);
+      // Apply Schlegeltransformation script
+      SchlegelTransformer st = new SchlegelTransformer(im);
 
-			System.out.println(st.getResult().toString());
+      pmWrapper.sendRequest(st);
+      // Since Transformer is not yet a future
+      Thread.sleep(2000);
+      System.out.println("Result is:");
+      System.out.println("------------");
+      System.out.println(st.getResult().toString());
+      System.out.println("------------");
+      pmWrapper.stop();
+      Thread.sleep(2000);
 
-			pmWrapper.stop();
-			Thread.sleep(2000);
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			p.destroy();
-			try {
-				s.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-	}
+  }
 
 }
