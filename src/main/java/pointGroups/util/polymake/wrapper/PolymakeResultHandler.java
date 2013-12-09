bdab3cc85e53b6728b3384c437fb9ac20a2e9b28
Alex <a.steen@fu-berlin.de>
2013-12-09 01:04:47 +0100

    implemented result fetch loop in polymake wrapper package

--- src/main/java/pointGroups/util/polymake/wrapper/PolymakeResultHandler.java
+++ src/main/java/pointGroups/util/polymake/wrapper/PolymakeResultHandler.java
@@ -1,17 +1,37 @@
 package pointGroups.util.polymake.wrapper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 class PolymakeResultHandler implements Runnable {
+	private final BufferedReader res;
+	private final PolymakeWrapper wrapper;
 
-	public PolymakeResultHandler(PolymakeWrapper polymakeWrapper, InputStream inputStream) {
-		// TODO Auto-generated constructor stub
+	public PolymakeResultHandler(PolymakeWrapper polymakeWrapper,
+			InputStream inputStream) {
+		this.res = new BufferedReader(new InputStreamReader(inputStream));
+		this.wrapper = polymakeWrapper;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		String output;
+		StringBuilder sb = new StringBuilder();
+		try {
+			while ((output = this.res.readLine()) != null) {
+				// TODO: String constants
+				if (output.equals("__END__")) {
+					this.wrapper.onMessageReceived(sb.toString());
+					sb = new StringBuilder();
+				} else {
+					sb.append(output + "\n");
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
