780d9aa501cc69f21f958a8ed1863c0d4f0619a9
Alexander Steen <a.steen@fu-berlin.de>
2013-12-09 11:36:49 +0100

    Added controlled shutdown of concurrent listeners

--- src/main/java/pointGroups/util/polymake/wrapper/PolymakeErrorStreamHandler.java
+++ src/main/java/pointGroups/util/polymake/wrapper/PolymakeErrorStreamHandler.java
@@ -6,24 +6,38 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
-class PolymakeErrorStreamHandler implements Runnable {
-	final Logger logger = Logger.getLogger(PolymakeErrorStreamHandler.class
-			.getName());
-	private final BufferedReader err;
 
-	PolymakeErrorStreamHandler(InputStream errorStream) {
-		this.err = new BufferedReader(new InputStreamReader(errorStream));
-	}
+class PolymakeErrorStreamHandler
+  implements Runnable
+{
+  final Logger logger =
+      Logger.getLogger(PolymakeErrorStreamHandler.class.getName());
+  private final BufferedReader err;
+  private final PolymakeWrapper wrapper;
 
-	@Override
-	public void run() {
-		String err;
-		try {
-			while ((err = this.err.readLine()) != null) {
-				logger.severe(err);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  PolymakeErrorStreamHandler(PolymakeWrapper polymakeWrapper,
+      InputStream errorStream) {
+    this.err = new BufferedReader(new InputStreamReader(errorStream));
+    this.wrapper = polymakeWrapper;
+  }
+
+  @Override
+  public void run() {
+    String err;
+    try {
+      while ((err = this.err.readLine()) != null) {
+        logger.severe(err);
+      }
+    }
+    catch (IOException e) {
+      if (this.wrapper.isRunning()) {
+        // Socket should not be closed (or some other error occured)
+        logger.severe("Socket was closed or another IO error occured");
+        logger.fine(e.getMessage());
+      }
+      else {
+        logger.info("Socket was closed itentionally, Resulthandler is terminating");
+      }
+    }
+  }
 }
--- src/main/java/pointGroups/util/polymake/wrapper/PolymakeResultHandler.java
+++ src/main/java/pointGroups/util/polymake/wrapper/PolymakeResultHandler.java
@@ -4,34 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
-class PolymakeResultHandler implements Runnable {
-	private final BufferedReader res;
-	private final PolymakeWrapper wrapper;
 
-	public PolymakeResultHandler(PolymakeWrapper polymakeWrapper,
-			InputStream inputStream) {
-		this.res = new BufferedReader(new InputStreamReader(inputStream));
-		this.wrapper = polymakeWrapper;
-	}
+class PolymakeResultHandler
+  implements Runnable
+{
+  private final Logger logger =
+      Logger.getLogger(PolymakeResultHandler.class.getName());
+  private final BufferedReader res;
+  private final PolymakeWrapper wrapper;
 
-	@Override
-	public void run() {
-		String output;
-		StringBuilder sb = new StringBuilder();
-		try {
-			while ((output = this.res.readLine()) != null) {
-				// TODO: String constants
-				if (output.equals("__END__")) {
-					this.wrapper.onMessageReceived(sb.toString());
-					sb = new StringBuilder();
-				} else {
-					sb.append(output + "\n");
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  public PolymakeResultHandler(PolymakeWrapper polymakeWrapper,
+      InputStream inputStream) {
+    this.res = new BufferedReader(new InputStreamReader(inputStream));
+    this.wrapper = polymakeWrapper;
+  }
 
+  @Override
+  public void run() {
+    String output;
+    StringBuilder sb = new StringBuilder();
+    try {
+      while ((output = this.res.readLine()) != null) {
+        // TODO: String constants
+        if (output.equals("__END__")) {
+          this.wrapper.onMessageReceived(sb.toString());
+          sb = new StringBuilder();
+        }
+        else {
+          sb.append(output + "\n");
+        }
+      }
+    }
+    catch (IOException e) {
+      if (this.wrapper.isRunning()) {
+        // Socket should not be closed (or some other error occured)
+        logger.severe("Socket was closed or another IO error occured");
+        logger.fine(e.getMessage());
+      }
+      else {
+        logger.info("Socket was closed itentionally, Resulthandler is terminating");
+      }
+    }
+  }
 }
