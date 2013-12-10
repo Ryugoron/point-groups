ad820406963370470294c3cc7c5087e041eac18e
Alex <a.steen@fu-berlin.de>
2013-12-10 14:38:41 +0100

    added documentation of PolymakeResultHandler

--- src/main/java/pointGroups/util/polymake/wrapper/PolymakeResultHandler.java
+++ src/main/java/pointGroups/util/polymake/wrapper/PolymakeResultHandler.java
@@ -7,6 +7,15 @@ import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 
+/**
+ * A {@link Runnable} that is used by {@link PolymakeWrapper} to listen on
+ * polymake's output and invoke
+ * {@link PolymakeWrapper#onMessageReceived(String)} when a message was
+ * recevied.
+ * 
+ * @author Alex
+ * @see PolymakeWrapper
+ */
 class PolymakeResultHandler
   implements Runnable
 {
@@ -15,6 +24,13 @@ class PolymakeResultHandler
   private final BufferedReader res;
   private final PolymakeWrapper wrapper;
 
+  /**
+   * Create a new {@link PolymakeResultHandler} runnable to listen for
+   * <code>polymakeWrapper</code> on the input stream <code>inputStream</code>.
+   * 
+   * @param polymakeWrapper The wrapped source of the responses
+   * @param inputStream The stream to listen on
+   */
   public PolymakeResultHandler(PolymakeWrapper polymakeWrapper,
       InputStream inputStream) {
     this.res = new BufferedReader(new InputStreamReader(inputStream));
@@ -23,12 +39,16 @@ class PolymakeResultHandler
 
   @Override
   public void run() {
+    // Continously listen on the input stream for responses by polymake.
+    // Invoke PolymakeWrapper#onMessageReceived on new response.
     String output;
     StringBuilder sb = new StringBuilder();
     try {
       while ((output = this.res.readLine()) != null) {
         // TODO: String constants
-        if (output.equals("__END__")) {
+        if (output.equals("__END__")) { // If the symbol __END__ occured, the
+                                        // response message is complete and can
+                                        // be forwarded.
           this.wrapper.onMessageReceived(sb.toString());
           sb = new StringBuilder();
         }
@@ -44,6 +64,7 @@ class PolymakeResultHandler
         logger.fine(e.getMessage());
       }
       else {
+        // No Error occured, polymake was shut down
         logger.info("Socket was closed itentionally, Resulthandler is terminating");
       }
     }
