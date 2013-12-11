dcbc1969edfe3e0621318d4881cad30c92d5578f
Nadja <scharfn@zedat.fu-berlin.de>
2013-12-11 19:50:56 +0100

    SchlegelTransformer extends AbstractTransformer<Schlegel> finished but not tested

--- src/main/java/pointGroups/util/AbstractTransformer.java
+++ src/main/java/pointGroups/util/AbstractTransformer.java
@@ -4,68 +4,65 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+public abstract class AbstractTransformer<E> implements Transformer<E> {
 
-public abstract class AbstractTransformer<E>
-  implements Transformer<E>
-{
+	private E result;
+	private volatile boolean done;
+	// has to be protected, otherwise transformResultString can't use it
+	protected volatile String resultString;
 
-  private E result;
-  private volatile boolean done;
-  private volatile String resultString;
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override
-  public boolean cancel(boolean mayInterruptIfRunning) {
-    throw new UnsupportedOperationException();
-  }
+	@Override
+	public void setResultString(String resultString) {
+		this.resultString = resultString;
+		this.notifyAll();
+	}
 
-  @Override
-  public void setResultString(String resultString) {
-    this.resultString = resultString;
-    this.notifyAll();
-  }
+	@Override
+	public E get() throws InterruptedException, ExecutionException {
+		synchronized (this) {
+			while (resultString == null) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					// Nothing to do. Still waiting for setResult().
+				}
+			}
+		}
+		if (result == null) {
+			this.result = transformResultString();
+			this.done = true;
+		}
+		return result;
+	}
 
-  @Override
-  public E get()
-    throws InterruptedException, ExecutionException {
-    synchronized (this) {
-      while (resultString == null) {
-        try {
-          this.wait();
-        }
-        catch (InterruptedException e) {
-          // Nothing to do. Still waiting for setResult().
-        }
-      }
-    }
-    if (result == null) {
-      this.result = transformResultString();
-      this.done = true;
-    }
-    return result;
-  }
+	/**
+	 * Transforms result string of the
+	 * {@link Transformer#setResultString(String)} to the internal
+	 * representation.
+	 * 
+	 * @return Internal representation of the external calculation.
+	 */
+	abstract protected E transformResultString();
 
-  /**
-   * Transforms result string of the
-   * {@link Transformer#setResultString(String)} to the internal representation.
-   * 
-   * @return Internal representation of the external calculation.
-   */
-  abstract protected E transformResultString();
+	@Override
+	public E get(long timeout, TimeUnit unit) throws InterruptedException,
+			ExecutionException, TimeoutException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override
-  public E get(long timeout, TimeUnit unit)
-    throws InterruptedException, ExecutionException, TimeoutException {
-    throw new UnsupportedOperationException();
-  }
+	@Override
+	public boolean isCancelled() {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override
-  public boolean isCancelled() {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public boolean isDone() {
-    return done;
-  }
+	@Override
+	public boolean isDone() {
+		return done;
+	}
 
 }
--- src/main/java/pointGroups/util/polymake/SchlegelTransformer.java
+++ src/main/java/pointGroups/util/polymake/SchlegelTransformer.java
@@ -2,6 +2,8 @@ package pointGroups.util.polymake;
 
 import pointGroups.geometry.Pair;
 import pointGroups.geometry.Point;
+import pointGroups.geometry.Point3D;
+import pointGroups.geometry.Schlegel;
 import pointGroups.util.AbstractTransformer;
 
 import java.util.Collection;
@@ -10,7 +12,7 @@ import java.util.Collection;
  * Transforms points into a script as string for computing schlegel vertices and edges
  * @author Nadja, Simon
  */
-public class SchlegelTransformer<Schlegel>
+public class SchlegelTransformer
   extends AbstractTransformer<Schlegel>
 {
   
@@ -72,42 +74,28 @@ public class SchlegelTransformer<Schlegel>
     return matrix.toString();
   }
 
-  @Override
-  protected Schlegel transformResultString() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
 	@Override
-	public Schlegel getResult() throws PolymakeOutputException {
-		while (result == null) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	public Schlegel transformResultString() {
 		StringBuilder regex = new StringBuilder();
 		// minimum one 2D point or...
-		regex.append("([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\s[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\n)+|");
+		regex.append("([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)? [-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\n)+|");
 		// minimum one 3D point followed by...
-		regex.append("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\s[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\s[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\n)+");
+		regex.append("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)? [-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)? [-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\n)+");
 		// a $\n...
 		regex.append("\\$\\n");
 		// minimum one edge
-		regex.append("(\\{[0-9]+\\s[0-9]+\\}\\n)+");
+		regex.append("(\\{[0-9]+ [0-9]+\\}\\n)+");
 
 		// test if result is formatted correct
-		if (!result.matches(regex.toString())) {
-			throw new PolymakeOutputException(
-					"String set by setResult() does not match defined format for schlegel.");
+		if (!resultString.matches(regex.toString())) {
+			throw new PolymakeException(
+					"String set by setResultString() does not match defined format for schlegel.");
 		} else {
 		}
 		// splitting result string into two. One contains the points, the other
 		// the
 		// edges.
-		String[] pointsAndEdges = result.split("\\$\n");
+		String[] pointsAndEdges = resultString.split("\\$\n");
 		String pointsString = pointsAndEdges[0];
 		String edgesString = pointsAndEdges[1];
 
@@ -118,7 +106,7 @@ public class SchlegelTransformer<Schlegel>
 		for (int i = 0; i < splittedPointsString.length; i++) {
 			String str = splittedPointsString[i];
 			// ignore brackets and split into components
-			String[] compStr = str.substring(1, str.length() - 2).split("\\,");
+			String[] compStr = str.substring(1, str.length() - 2).split(" ");
 			if (compStr.length == 2) {
 				points[i] = new Point3D(Double.parseDouble(compStr[0]),
 						Double.parseDouble(compStr[1]), 0);
@@ -134,17 +122,14 @@ public class SchlegelTransformer<Schlegel>
 		}
 		// Store Edges as Array von Pair<Point3D,Point3D>
 		String[] splittedEdgesString = edgesString.split("\n");
-		// TODO das mit den Generics ist Mist. Geht das besser?
 		Pair<Point3D, Point3D>[] edges = new Pair[splittedEdgesString.length];
 		// start iteration with i = 1 because the first string after splitting
 		// is
 		// empty caused by leading \n
-		for (int i = 1; i < splittedEdgesString.length; i++) {
+		for (int i = 0; i < splittedEdgesString.length; i++) {
 			String str = splittedEdgesString[i];
 			// ignore brackets and split into components
-			String[] compStr = str.substring(1, str.length() - 2).split("\\,");
-			// TODO what if there are more or less than two components for an
-			// edge?
+			String[] compStr = str.substring(1, str.length() - 2).split(" ");
 			Point3D from = points[Integer.valueOf(compStr[0])];
 			Point3D to = points[Integer.valueOf(compStr[1])];
 			edges[i] = new Pair<Point3D, Point3D>(from, to);
