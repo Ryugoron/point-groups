dcdfd8317900993ca8fabf44dfa80e66da052ae0
Marcel Ehrhradt <marehr@zedat.fu-berlin.de>
2013-12-18 18:51:22 +0100

    Added a TestCase for the event flow in the event system

---
+++ src/test/java/pointGroups/gui/event/EventFlowTest.java
@@ -0,0 +1,78 @@
+package pointGroups.gui.event;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import pointGroups.geometry.Point3D;
+import pointGroups.geometry.Symmetry;
+import pointGroups.geometry.symmetries.TetrahedralSymmetry;
+import pointGroups.gui.event.types.Symmetry3DChooseEvent;
+import pointGroups.gui.event.types.Symmetry3DChooseHandler;
+import pointGroups.gui.event.types.Symmetry3DChooseListener;
+
+
+public class EventFlowTest
+{
+  static int onSymmetry3DChooseEventCalled = 0;
+
+
+  public static class Main
+  {
+    EventDispatcher dispatcher = new EventDispatcher();
+  }
+
+
+  public static class SymmetryChooser
+  {
+    EventDispatcher dispatcher;
+
+    public SymmetryChooser(EventDispatcher dispatcher) {
+      this.dispatcher = dispatcher;
+    }
+
+    public void changeSymmetry(Symmetry<Point3D, ?> symmetry) {
+      dispatcher.dispatchEvent(new Symmetry3DChooseEvent(symmetry));
+    }
+  }
+
+
+  public static class PointPicker
+    implements Symmetry3DChooseListener
+  {
+    EventDispatcher dispatcher;
+    Symmetry<Point3D, ?> symmetry;
+
+    public PointPicker(EventDispatcher dispatcher) {
+      dispatcher.addSymmetry3DChooseHandler(new Symmetry3DChooseHandler(this));
+
+      this.dispatcher = dispatcher;
+    }
+
+    @Override
+    public void onSymmetry3DChooseEvent(Symmetry3DChooseEvent event) {
+      onSymmetry3DChooseEventCalled++;
+
+      symmetry = event.getSymmetry3D();
+
+      assertEquals("the event should contain the right symmetry",
+          TetrahedralSymmetry.get(), symmetry);
+    }
+  }
+
+  @Test
+  public void testWorkflow() {
+    Main main = new Main();
+    SymmetryChooser chooser = new SymmetryChooser(main.dispatcher);
+    PointPicker picker = new PointPicker(main.dispatcher);
+
+    chooser.changeSymmetry(TetrahedralSymmetry.get());
+
+    assertEquals(
+        "PointPicker#onSymmetry3DChooseEvent should be called at least once",
+        1, onSymmetry3DChooseEventCalled);
+
+    assertEquals("PointPicker should have the right symmetry assigned",
+        TetrahedralSymmetry.get(), picker.symmetry);
+  }
+}
