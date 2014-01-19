05d89bd04e8fb2bd1bb45997031235dea17238de
Max Wisniewski <max.wisniewski@fu-berlin.de>
2014-01-19 22:24:21 +0100

    Fundamental can already compute a triangular fundamental domain. But it can not be represented in the correct dimension

--- src/main/perl/fundamental.pl
+++ src/main/perl/fundamental.pl
@@ -1,14 +1,52 @@
 use application "polytope";
 
 # Points of the point group
-my $points = new Matrix<Rational>([[1,-1,-1],[1,1,-1],[1,-1,1],[1,1,1],[1,0,0]]);
+# Test is a cube with fixed first component.
+my $points = new Matrix<Rational>([[1,1,1,1],[1,1,1,-1],[1,1,-1,1],[1,-1,1,1],[1,1,-1,-1],[1,-1,1,-1],[1,-1,-1,1],[1,-1,-1,-1]]);
 
 my $v = new VoronoiDiagram(SITES=>$points);
 
-# Selects the first facet of the voronoi diagram
-my $vert = $v->VERTICES_IN_FACETS->[0];
+#
+# Should be only one positioned at ~(0,0,0) otherwise error
+#
+print $v->VORONOI_VERTICES;
+print "\n";
 
-# Prints all Vertices in the first facet
-for ($count = 0; $count < $vert->size; $count++){
-   print $v->VERTICES->[$vert[$count]];
-}
+#
+# Prints the the adjacent voronoi cells.
+# For the first node
+#
+my $adj = $v->DUAL_GRAPH->ADJACENCY->adjacent_nodes(0);
+print $adj;
+print "\n";
+
+#
+# Computes the rays of the voronoi cell,
+# we assume there are only triangualar fundemantal regions
+# at this point
+#
+
+# First save the sites
+my $s1 = $points->[$adj->[0]];
+my $s2 = $points->[$adj->[1]];
+my $s3 = $points->[$adj->[2]];
+
+# Compute the points on the edge * 2, because polymake cannot divide a vector by a scalar.
+my $p1 = $s1 + $s2;
+my $p2 = $s2 + $s3;
+my $p3 = $s3 + $s1;
+
+#
+# We create a new polytope from these points, such that they are normalized.
+# and print them.
+#
+my $poly = new Polytope(POINTS=>[$p1,$p2,$p3,[2,0,0,0]]);
+print "----------\n";
+print $poly->VERTICES;
+print "---------\n";
+print $poly->FACETS;
+print "---------\n";
+
+my $schlegel = $poly->SCHLEGEL_DIAGRAM(FACET=>4);
+
+print $schlegel->VERTICES;
