ddb913883c3b3c09a2a6d84a07f12991b325db81
Alex <a.steen@fu-berlin.de>
2014-01-20 00:32:02 +0100

    added panels for symmetries and subgroups and enabled firing of events

--- src/main/java/pointGroups/gui/symchooser/SymmetryPanel.java
+++ src/main/java/pointGroups/gui/symchooser/SymmetryPanel.java
@@ -3,7 +3,8 @@ package pointGroups.gui.symchooser;
 import java.awt.Dimension;
 import java.util.logging.Logger;
 
-import javax.swing.JPanel;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -11,8 +12,13 @@ import javax.swing.event.ChangeListener;
 import pointGroups.geometry.Point;
 import pointGroups.geometry.Point3D;
 import pointGroups.geometry.Point4D;
+import pointGroups.geometry.Symmetry;
+import pointGroups.geometry.symmetries.IcosahedralSymmetry;
+import pointGroups.geometry.symmetries.OctahedralSymmetry;
+import pointGroups.geometry.symmetries.TetrahedralSymmetry;
 import pointGroups.gui.event.EventDispatcher;
 import pointGroups.gui.event.types.DimensionSwitchEvent;
+import pointGroups.gui.symchooser.elements.SymmetryList;
 
 
 public class SymmetryPanel
@@ -20,22 +26,29 @@ public class SymmetryPanel
 {
   private static final long serialVersionUID = 8455433801260981217L;
   private final Logger logger = Logger.getLogger(this.getClass().getName());
-  protected final JPanel symmetries3DPanel, symmetries4DPanel;
+
+  protected final JList<Symmetry<Point3D, ?>> symmetries3DPanel;
+  protected final JList<Symmetry<Point4D, ?>> symmetries4DPanel;
+  protected final DefaultListModel<Symmetry<Point3D, ?>> symmetries3D;
+  protected final DefaultListModel<Symmetry<Point4D, ?>> symmetries4D;
 
   protected final EventDispatcher dispatcher = EventDispatcher.get();
-  protected final SubgroupPanel subgroupPanel;
 
   public SymmetryPanel(final SubgroupPanel subgroupPanel) {
     super();
-    this.subgroupPanel = subgroupPanel;
 
-    this.symmetries3DPanel = new JPanel();
-    this.symmetries4DPanel = new JPanel();
+    this.symmetries3D = new DefaultListModel<>();
+    this.symmetries4D = new DefaultListModel<>();
+    this.symmetries3DPanel =
+        new SymmetryList<Point3D>(this.symmetries3D, subgroupPanel);
+    this.symmetries4DPanel =
+        new SymmetryList<Point4D>(this.symmetries4D, subgroupPanel);
 
     Dimension dim = new Dimension(250, 0);
     setMinimumSize(dim);
     setPreferredSize(dim);
-    // fillPanelsWithSymmetries();
+
+    this.fillPanelsWithSymmetries();
 
     add("3D", this.symmetries3DPanel);
     add("4D", this.symmetries4DPanel);
@@ -68,4 +81,11 @@ public class SymmetryPanel
     });
   }
 
+  private void fillPanelsWithSymmetries() {
+    // 3D Symmetries
+    this.symmetries3D.addElement(TetrahedralSymmetry.get());
+    this.symmetries3D.addElement(OctahedralSymmetry.get());
+    this.symmetries3D.addElement(IcosahedralSymmetry.get());
+    // 4D Symmetries
+  }
 }
