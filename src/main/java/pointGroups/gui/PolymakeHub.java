0780106421c6e1328e5e390c0b0a97662352eb21
Alex <a.steen@fu-berlin.de>
2014-01-21 19:47:15 +0100

    added request sending to polymakeHub

--- src/main/java/pointGroups/gui/PolymakeHub.java
+++ src/main/java/pointGroups/gui/PolymakeHub.java
@@ -1,5 +1,8 @@
 package pointGroups.gui;
 
+import java.util.Collection;
+
+import pointGroups.geometry.Point;
 import pointGroups.geometry.Point3D;
 import pointGroups.geometry.Point4D;
 import pointGroups.geometry.Symmetry;
@@ -9,6 +12,7 @@ import pointGroups.gui.event.types.Symmetry3DChooseEvent;
 import pointGroups.gui.event.types.Symmetry3DChooseHandler;
 import pointGroups.gui.event.types.Symmetry4DChooseEvent;
 import pointGroups.gui.event.types.Symmetry4DChooseHandler;
+import pointGroups.util.polymake.SchlegelTransformer;
 import pointGroups.util.polymake.wrapper.PolymakeWrapper;
 
 
@@ -30,17 +34,23 @@ public class PolymakeHub
     final int dimension = event.getDimension();
     final double[] coords = event.getCoordinates();
 
+    Collection<? extends Point> images = null;
     if (dimension == 3) {
       if (last3DSymmetry != null) {
         Point3D p = new Point3D(coords[0], coords[1], coords[2]);
-        last3DSymmetry.images(p, lastSubgroup);
+        images = last3DSymmetry.images(p, lastSubgroup);
       }
     }
     else if (dimension == 4) {
       if (last4DSymmetry != null) {
         Point4D p = new Point4D(coords[0], coords[1], coords[2], coords[3]);
-        last4DSymmetry.images(p, lastSubgroup);
+        images = last4DSymmetry.images(p, lastSubgroup);
       }
+    }
+
+    if (images != null) {
+      SchlegelTransformer st = new SchlegelTransformer(images);
+      pmWrapper.sendRequest(st);
     }
   }
 
