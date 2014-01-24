0ef6db6dd1425d573b3a3537e1542bc87821bce6
Marcel Ehrhradt <marehr@zedat.fu-berlin.de>
2014-01-24 18:28:11 +0100

    New JRealityUtility: transform an edge list and point list into a jReality geometry object

---
+++ src/main/java/pointGroups/util/jreality/JRealityUtility.java
@@ -0,0 +1,54 @@
+package pointGroups.util.jreality;
+
+import pointGroups.geometry.Edge;
+import pointGroups.geometry.Point;
+import de.jreality.geometry.IndexedFaceSetFactory;
+import de.jreality.scene.Geometry;
+
+
+public class JRealityUtility
+{
+
+  public static double[][] convertPoints(Point[] points) {
+    double[][] points_ = new double[points.length][];
+
+    int i = 0;
+    for (Point point : points) {
+      points_[i++] = point.getComponents();
+    }
+
+    return points_;
+  }
+
+  public static int[][] convertEdges(Edge<Integer, Integer>[] edges) {
+    int[][] edges_ = new int[edges.length][];
+
+    int i = 0;
+    for (Edge<Integer, Integer> edge : edges) {
+      int u = edge.left, v = edge.right;
+
+      edges_[i++] = new int[] { u, v };
+    }
+
+    return edges_;
+  }
+
+  public static Geometry generateGraph(Point[] points,
+      Edge<Integer, Integer>[] edges) {
+    return generateGraph(convertPoints(points), convertEdges(edges));
+  }
+
+  public static Geometry generateGraph(double[][] points, int[][] edges) {
+    IndexedFaceSetFactory ifsf = new IndexedFaceSetFactory();
+
+    ifsf.setVertexCount(points.length);
+    ifsf.setVertexCoordinates(points);
+
+    ifsf.setEdgeCount(edges.length);
+    ifsf.setEdgeIndices(edges);
+    ifsf.update();
+
+    return ifsf.getGeometry();
+  }
+
+}
