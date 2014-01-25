db29401f8771c4fab10b7c58840b895c20063b36
Marcel Ehrhradt <marehr@zedat.fu-berlin.de>
2014-01-25 16:10:22 +0100

    SchlegelTransformer: dryed testcase

--- src/test/java/pointGroups/util/polymake/SchlegelTransformerTest.java
+++ src/test/java/pointGroups/util/polymake/SchlegelTransformerTest.java
@@ -6,7 +6,6 @@ import java.util.concurrent.ExecutionException;
 
 import junit.framework.TestCase;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import pointGroups.geometry.Point;
@@ -17,10 +16,16 @@ import pointGroups.geometry.Schlegel;
 public class SchlegelTransformerTest
   extends TestCase
 {
+  public static final String POLYMAKE_2D_RESULT =
+      "0.4321 -0.1234\n1 3\n4 5.5\n$\n{1 0}\n{1 2}\n{0 2}\n";
+
+  public static final String POLYMAKE_3D_RESULT =
+      "0.4321 -0.1234 0.0\n1 3 5\n4 5.5 -2\n$\n{1 0}\n{1 2}\n{0 2}\n";
 
   Collection<Point> points3D;
   SchlegelTransformer st;
 
+  @Override
   public void setUp() {
     points3D = new ArrayList<Point>();
     points3D.add(new Point3D(0, 0, 0));
@@ -31,82 +36,28 @@ public class SchlegelTransformerTest
 
   @Test
   public void testTransformResultString2DSchlegel() {
-    String result = "0.4321 -0.1234\n1 3\n4 5.5\n$\n{1 0}\n{1 2}\n{0 2}\n";
-    st.setResultString(result);
+    st.setResultString(POLYMAKE_2D_RESULT);
     Schlegel schlegel = st.transformResultString();
-    Assert.assertEquals(schlegel.points[0].i, 0.4321, 0);
-    Assert.assertEquals(schlegel.points[0].j, -0.1234, 0);
-    Assert.assertEquals(schlegel.points[1].i, 1, 0);
-    Assert.assertEquals(schlegel.points[1].j, 3, 0);
-    Assert.assertEquals(schlegel.points[2].i, 4, 0);
-    Assert.assertEquals(schlegel.points[2].j, 5.5, 0);
-    Assert.assertEquals(schlegel.edgesViaPoints[0].left, schlegel.points[1]);
-    Assert.assertEquals(schlegel.edgesViaPoints[0].right, schlegel.points[0]);
-    Assert.assertEquals(schlegel.edgesViaPoints[1].left, schlegel.points[1]);
-    Assert.assertEquals(schlegel.edgesViaPoints[1].right, schlegel.points[2]);
-    Assert.assertEquals(schlegel.edgesViaPoints[2].left, schlegel.points[0]);
-    Assert.assertEquals(schlegel.edgesViaPoints[2].right, schlegel.points[2]);
-    Assert.assertEquals(schlegel.edgesViaIndices[0].left, 1, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[0].right, 0, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[1].left, 1, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[1].right, 2, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[2].left, 0, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[2].right, 2, 0);
+
+    assert2DResults(schlegel);
   }
 
   @Test
   public void testTransformResultString3DSchlegel() {
-    String result =
-        "0.4321 -0.1234 0.0\n1 3 5\n4 5.5 -2\n$\n{1 0}\n{1 2}\n{0 2}\n";
-    st.setResultString(result);
+    st.setResultString(POLYMAKE_3D_RESULT);
     Schlegel schlegel = st.transformResultString();
-    Assert.assertEquals(schlegel.points[0].i, 0.4321, 0);
-    Assert.assertEquals(schlegel.points[0].j, -0.1234, 0);
-    Assert.assertEquals(schlegel.points[0].k, 0, 0);
-    Assert.assertEquals(schlegel.points[1].i, 1, 0);
-    Assert.assertEquals(schlegel.points[1].j, 3, 0);
-    Assert.assertEquals(schlegel.points[1].k, 5, 0);
-    Assert.assertEquals(schlegel.points[2].i, 4, 0);
-    Assert.assertEquals(schlegel.points[2].j, 5.5, 0);
-    Assert.assertEquals(schlegel.points[2].k, -2, 0);
-    Assert.assertEquals(schlegel.edgesViaPoints[0].left, schlegel.points[1]);
-    Assert.assertEquals(schlegel.edgesViaPoints[0].right, schlegel.points[0]);
-    Assert.assertEquals(schlegel.edgesViaPoints[1].left, schlegel.points[1]);
-    Assert.assertEquals(schlegel.edgesViaPoints[1].right, schlegel.points[2]);
-    Assert.assertEquals(schlegel.edgesViaPoints[2].left, schlegel.points[0]);
-    Assert.assertEquals(schlegel.edgesViaPoints[2].right, schlegel.points[2]);
-    Assert.assertEquals(schlegel.edgesViaIndices[0].left, 1, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[0].right, 0, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[1].left, 1, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[1].right, 2, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[2].left, 0, 0);
-    Assert.assertEquals(schlegel.edgesViaIndices[2].right, 2, 0);
+
+    assert3DResults(schlegel);
   }
 
   @Test
   public void testSetAndGetSeq()
     throws InterruptedException, ExecutionException {
-    String resultString =
-        "0.4321 -0.1234 0.0\n1 3 5\n4 5.5 -2\n$\n{1 0}\n{1 2}\n{0 2}\n";
-    st.setResultString(resultString);
+    st.setResultString(POLYMAKE_3D_RESULT);
     Schlegel schlegel = st.get();
-    Assert.assertEquals(schlegel.points[0].i, 0.4321, 0);
-    Assert.assertEquals(schlegel.points[0].j, -0.1234, 0);
-    Assert.assertEquals(schlegel.points[0].k, 0, 0);
-    Assert.assertEquals(schlegel.points[1].i, 1, 0);
-    Assert.assertEquals(schlegel.points[1].j, 3, 0);
-    Assert.assertEquals(schlegel.points[1].k, 5, 0);
-    Assert.assertEquals(schlegel.points[2].i, 4, 0);
-    Assert.assertEquals(schlegel.points[2].j, 5.5, 0);
-    Assert.assertEquals(schlegel.points[2].k, -2, 0);
-    Assert.assertSame(schlegel.points[1], schlegel.edgesViaPoints[0].left);
-    Assert.assertSame(schlegel.points[0], schlegel.edgesViaPoints[0].right);
-    Assert.assertSame(schlegel.points[1], schlegel.edgesViaPoints[1].left);
-    Assert.assertSame(schlegel.points[2], schlegel.edgesViaPoints[1].right);
-    Assert.assertSame(schlegel.points[0], schlegel.edgesViaPoints[2].left);
-    Assert.assertSame(schlegel.points[2], schlegel.edgesViaPoints[2].right);
 
-    Assert.assertTrue(st.isDone());
+    assert3DResults(schlegel);
+    assertTrue(st.isDone());
   }
 
   @Test
@@ -114,16 +65,9 @@ public class SchlegelTransformerTest
     throws InterruptedException, ExecutionException {
     new Thread(new GetRunnable(st)).start();
     Schlegel schlegel = st.get();
-    Assert.assertEquals(0, schlegel.points[0].i, 0);
-    Assert.assertEquals(0, schlegel.points[0].j, 0);
-    Assert.assertEquals(0, schlegel.points[0].k, 0);
-    Assert.assertEquals(1, schlegel.points[1].i, 0);
-    Assert.assertEquals(1, schlegel.points[1].j, 0);
-    Assert.assertEquals(1, schlegel.points[1].k, 0);
-    Assert.assertSame(schlegel.points[0], schlegel.edgesViaPoints[0].left);
-    Assert.assertSame(schlegel.points[1], schlegel.edgesViaPoints[0].right);
 
-    Assert.assertTrue(st.isDone());
+    assert3DResults(schlegel);
+    assertTrue(st.isDone());
   }
 
 
@@ -131,12 +75,13 @@ public class SchlegelTransformerTest
     implements Runnable
   {
 
-    private SchlegelTransformer st;
+    private final SchlegelTransformer st;
 
     public GetRunnable(SchlegelTransformer st) {
       this.st = st;
     }
 
+    @Override
     public void run() {
       try {
         // this is hopefully enough time such that st.get() will be executed
@@ -146,19 +91,47 @@ public class SchlegelTransformerTest
         }
       }
       catch (InterruptedException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
-      st.setResultString("0 0 0\n1 1 1\n$\n{0 1}\n");
+      st.setResultString(POLYMAKE_3D_RESULT);
     }
   }
 
   @Test
-  public void testToString() {
+  public void testToScript() {
     String script = st.toScript();
     String desiredScript =
-        "use application \"polytope\";my $mat=new Matrix<Rational>([[1,0.0,0.0,0.0],[1,1.1,0.0,0.0],[1,0.0,-1.23,1.0]]);my $p = new Polytope(POINTS=>$mat);my $schlegelverts = $p->SCHLEGEL_DIAGRAM->VERTICES;my $edges = $p->GRAPH->EDGES;my $v = \"$schlegelverts\";my $e = \"$edges\";print $v.\"$\".$e";
-    Assert.assertTrue(script.equals(desiredScript));
+        "use application \"polytope\";my $mat=new Matrix<Rational>([[1,0.0,0.0,0.0],[1,1.1,0.0,0.0],[1,0.0,-1.23,1.0]]);my $p = new Polytope(POINTS=>$mat);my $schlegelverts = $p->SCHLEGEL_DIAGRAM->VERTICES;my $edges = $p->GRAPH->EDGES;my $v = \"$schlegelverts\";my $e = \"$edges\";print $v.\"\\$\\n\".$e";
+
+    assertEquals(desiredScript, script);
   }
 
+  public void assert2DResults(Schlegel schlegel) {
+    assertEquals(schlegel.points[0].i, 0.4321, 0);
+    assertEquals(schlegel.points[0].j, -0.1234, 0);
+    assertEquals(schlegel.points[1].i, 1, 0);
+    assertEquals(schlegel.points[1].j, 3, 0);
+    assertEquals(schlegel.points[2].i, 4, 0);
+    assertEquals(schlegel.points[2].j, 5.5, 0);
+    assertEquals(schlegel.edgesViaPoints[0].left, schlegel.points[1]);
+    assertEquals(schlegel.edgesViaPoints[0].right, schlegel.points[0]);
+    assertEquals(schlegel.edgesViaPoints[1].left, schlegel.points[1]);
+    assertEquals(schlegel.edgesViaPoints[1].right, schlegel.points[2]);
+    assertEquals(schlegel.edgesViaPoints[2].left, schlegel.points[0]);
+    assertEquals(schlegel.edgesViaPoints[2].right, schlegel.points[2]);
+    assertEquals(schlegel.edgesViaIndices[0].left, 1, 0);
+    assertEquals(schlegel.edgesViaIndices[0].right, 0, 0);
+    assertEquals(schlegel.edgesViaIndices[1].left, 1, 0);
+    assertEquals(schlegel.edgesViaIndices[1].right, 2, 0);
+    assertEquals(schlegel.edgesViaIndices[2].left, 0, 0);
+    assertEquals(schlegel.edgesViaIndices[2].right, 2, 0);
+  }
+
+  public void assert3DResults(Schlegel schlegel) {
+    assert2DResults(schlegel);
+
+    assertEquals(schlegel.points[0].k, 0, 0);
+    assertEquals(schlegel.points[1].k, 5, 0);
+    assertEquals(schlegel.points[2].k, -2, 0);
+  }
 }
