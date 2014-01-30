11244eaa1447ced8f4657080fcc4a1a2e842d9e4
Nadja <scharfn@zedat.fu-berlin.de>
2014-01-30 12:38:57 +0100

    Deleted format test of resultString via regexp.

--- src/main/java/pointGroups/util/polymake/SchlegelTransformer.java
+++ src/main/java/pointGroups/util/polymake/SchlegelTransformer.java
@@ -89,68 +89,85 @@ public class SchlegelTransformer
 
   @Override
   public SchlegelCompound transformResultString() {
-    StringBuilder regex = new StringBuilder();
-    // minimum one 2D point or...
-    regex.append("(([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)? [-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\n)+|");
-    // minimum one 3D point followed by...
-    regex.append("([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)? [-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)? [-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\n)+)");
-    // a $\n...
-    regex.append("\\$\\n");
-    // minimum one edge
-    regex.append("(\\{[0-9]+ [0-9]+\\}\\n)+");
-
-    // test if result is formatted correct
-    if (!resultString.matches(regex.toString())) { throw new PolymakeOutputException(
-        "String set by setResultString() does not match defined format for schlegel."); }
+    
     // splitting result string into two. One contains the points, the other
     // the
     // edges.
-    String[] pointsAndEdges = resultString.split("\\$\n");
-    String pointsString = pointsAndEdges[0];
-    String edgesString = pointsAndEdges[1];
+    
+    String pointsString;
+    String edgesString;
+    try {
+      String[] pointsAndEdges = resultString.split("\\$\n");
+      pointsString = pointsAndEdges[0];
+      edgesString = pointsAndEdges[1];
+    }
+    catch (Exception e) {
+      logger.severe("Wrong Format of resultString. Probably missing \\$\n");
+      logger.fine(e.getMessage());
+      logger.fine("resultString was: " + resultString);
+      throw new PolymakeOutputException("Can't split Resultstring at \\$\n");
+    }
 
-    // splitting string containing points into one per point
-    String[] splittedPointsString = pointsString.split("\n");
     // transforming strings into Point-Objects
-    Point3D[] points = new Point3D[splittedPointsString.length];
-    for (int i = 0; i < splittedPointsString.length; i++) {
-      String str = splittedPointsString[i];
-      // ignore brackets and split into components
-      String[] compStr = str.substring(0, str.length()).split(" ");
-      if (compStr.length == 2) {
-        points[i] =
-            new Point3D(Double.parseDouble(compStr[0]),
-                Double.parseDouble(compStr[1]), 0);
-      }
-      else if (compStr.length == 3) {
-        points[i] =
-            new Point3D(Double.parseDouble(compStr[0]),
-                Double.parseDouble(compStr[1]), Double.parseDouble(compStr[2]));
-      }
-      else {
-        logger.severe(logger.getName() + ": point in resultString split in: " +
-            compStr.length + "components");
-        logger.fine(logger.getName() + ": resultString was: " + resultString);
+    Point3D[] points;
+    try {
+      // splitting string containing points into one per point
+      String[] splittedPointsString = pointsString.split("\n");
+      points = new Point3D[splittedPointsString.length];
+      for (int i = 0; i < splittedPointsString.length; i++) {
+        String str = splittedPointsString[i];
+        // ignore brackets and split into components
+        String[] compStr = str.substring(0, str.length()).split(" ");
+        if (compStr.length == 2) {
+          points[i] =
+              new Point3D(Double.parseDouble(compStr[0]),
+                  Double.parseDouble(compStr[1]), 0);
+        }
+        else if (compStr.length == 3) {
+          points[i] =
+              new Point3D(Double.parseDouble(compStr[0]),
+                  Double.parseDouble(compStr[1]), Double.parseDouble(compStr[2]));
+        }
+        else {
+          logger.severe("Point in resultString split in: " +
+              compStr.length + "components");
+          logger.fine("resultString was: " + resultString);
+        }
       }
     }
-    // Store Edges as Array von Pair<Point3D,Point3D>
-    String[] splittedEdgesString = edgesString.split("\n");
-    Edge<Point3D, Point3D>[] edges = new Edge[splittedEdgesString.length];
-    Edge<Integer, Integer>[] edgesindices =
-        new Edge[splittedEdgesString.length];
-    // start iteration with i = 1 because the first string after splitting
-    // is
-    // empty caused by leading \n
-    for (int i = 0; i < splittedEdgesString.length; i++) {
-      String str = splittedEdgesString[i];
-      // ignore brackets and split into components
-      String[] compStr = str.substring(1, str.length() - 1).split(" ");
-      int fromIndex = Integer.valueOf(compStr[0]);
-      int toIndex = Integer.valueOf(compStr[1]);
-      Point3D from = points[fromIndex];
-      Point3D to = points[toIndex];
-      edges[i] = new Edge<Point3D, Point3D>(from, to);
-      edgesindices[i] = new Edge<Integer, Integer>(fromIndex, toIndex);
+    catch (Exception e) {
+      logger.severe("Can't parse points in resultString.");
+      logger.fine(e.getMessage());
+      logger.fine("resultString was: " + resultString);
+      throw new PolymakeOutputException("Wrong Format of resultString.");
+    }
+    Edge<Point3D, Point3D>[] edges;
+    Edge<Integer, Integer>[] edgesindices;
+    try {
+      // Store Edges as Array von Pair<Point3D,Point3D> and as Array von Pair<Integer,Integer>
+      String[] splittedEdgesString = edgesString.split("\n");
+      edges = new Edge[splittedEdgesString.length];
+      edgesindices = new Edge[splittedEdgesString.length];
+      // start iteration with i = 1 because the first string after splitting
+      // is
+      // empty caused by leading \n
+      for (int i = 0; i < splittedEdgesString.length; i++) {
+        String str = splittedEdgesString[i];
+        // ignore brackets and split into components
+        String[] compStr = str.substring(1, str.length() - 1).split(" ");
+        int fromIndex = Integer.valueOf(compStr[0]);
+        int toIndex = Integer.valueOf(compStr[1]);
+        Point3D from = points[fromIndex];
+        Point3D to = points[toIndex];
+        edges[i] = new Edge<Point3D, Point3D>(from, to);
+        edgesindices[i] = new Edge<Integer, Integer>(fromIndex, toIndex);
+      }
+    }
+    catch (Exception e) {
+      logger.severe("Can't parse edges in resultString.");
+      logger.fine(e.getMessage());
+      logger.fine("resultString was: " + resultString);
+      throw new PolymakeOutputException("Wrong Format of resultString.");
     }
 
     return new SchlegelCompound(new Schlegel(points, edges, edgesindices),
