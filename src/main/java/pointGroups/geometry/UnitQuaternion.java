df07b7b3e85c83b045089fc1076abb98bcd2a80a
Alex <a.steen@fu-berlin.de>
2014-02-03 14:32:51 +0100

    Fixed bug in normalizing quaternions as well as some typos

--- src/main/java/pointGroups/geometry/UnitQuaternion.java
+++ src/main/java/pointGroups/geometry/UnitQuaternion.java
@@ -1,9 +1,9 @@
 package pointGroups.geometry;
 
 /**
- * A unit quaternions is a quaternion q with norm(q) = 1. Unit quaternions
- * represent rotations in R^3 (by conjugation) and can be used to rotate
- * {@link Point}s.
+ * A unit quaternion is a {@link Quaternion} q with norm(q) = 1. Unit
+ * quaternions represent rotations in R^3 (by conjugation) and can be used to
+ * rotate {@link Point}s.
  * 
  * @author Alex
  * @see Quaternion
@@ -14,15 +14,17 @@ public class UnitQuaternion
 
   /**
    * Creates a new {@link UnitQuaternion} object, which represents a normalized
-   * quaternions q, that is, norm(q) = 1. The parameters used to construct this
-   * object does not necessarily have to be normalized.
+   * quaternion q, that is, norm(q) = 1. The parameters used to construct this
+   * object do not necessarily have to be normalized, normalization is done
+   * automatically.
    * 
    * @param re Real part of q
    * @param i i-coordinate of q
    * @param j j-coordinate of q
    * @param k k-coordinate of q
    */
-  public UnitQuaternion(double re, double i, double j, double k) {
+  public UnitQuaternion(final double re, final double i, final double j,
+      final double k) {
     super(i, j, k, re, false);
   }
 
@@ -38,7 +40,8 @@ public class UnitQuaternion
    * @param unit set to true, if no normalization is required (i.e. the
    *          coordinates are a unit)
    */
-  protected UnitQuaternion(double re, double i, double j, double k, boolean unit) {
+  protected UnitQuaternion(final double re, final double i, final double j,
+      final double k, final boolean unit) {
     super(i, j, k, re, unit);
   }
 
@@ -48,7 +51,7 @@ public class UnitQuaternion
   }
 
   /**
-   * Due to eulers theorem, we can represent any rotation in R^3 using a single
+   * Due to euler's theorem, we can represent any rotation in R^3 using a single
    * unit quaternion which is uniquely determined by the rotation axis and the
    * rotation angle. This method generates a {@link UnitQuaternion} representing
    * the given rotation.
@@ -57,7 +60,8 @@ public class UnitQuaternion
    * @param axis The rotation axis (as Vector in R^3)
    * @return A {@link UnitQuaternion} representing the given rotation
    */
-  public static UnitQuaternion from3DRotation(double angle, Quaternion axis) {
+  public static UnitQuaternion from3DRotation(final double angle,
+      final Quaternion axis) {
     final double halfangle = angle / 2d;
 
     Quaternion p = Quaternion.fromDouble(Math.cos(halfangle));
@@ -75,8 +79,8 @@ public class UnitQuaternion
    * @param axis The rotation axis (as Vector in R^3)
    * @return A {@link UnitQuaternion} representing the given rotation
    */
-  public static UnitQuaternion
-      from3DRotation(double angle, UnitQuaternion axis) {
+  public static UnitQuaternion from3DRotation(final double angle,
+      final UnitQuaternion axis) {
     final double halfangle = angle / 2d;
 
     Quaternion p = Quaternion.fromDouble(Math.cos(halfangle));
@@ -85,11 +89,4 @@ public class UnitQuaternion
 
     return new UnitQuaternion(res.re, res.i, res.j, res.k, true);
   }
-
-  /*
-   * Still no idea how this works, have to look it up. public static
-   * Pair<UnitQuaternion, UnitQuaternion> from4DRotation(double angle) { return
-   * null; }
-   */
-
 }
