053a22fa9c77ae03925fb32250e5581b658ca16e
Alex <a.steen@fu-berlin.de>
2014-02-03 14:41:48 +0100

    help menu is not added to menuBar

--- src/main/java/pointGroups/gui/Menubar.java
+++ src/main/java/pointGroups/gui/Menubar.java
@@ -2,7 +2,6 @@ package pointGroups.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JCheckBoxMenuItem;
@@ -12,12 +11,12 @@ import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 
 import pointGroups.gui.event.EventDispatcher;
-import pointGroups.gui.event.types.ShowNextEvent;
 import pointGroups.gui.event.types.ShowCoordinateEvent;
 import pointGroups.gui.event.types.ShowFundamentalDomainEvent;
 import pointGroups.gui.event.types.ShowLogEvent;
-import pointGroups.gui.event.types.TutorialEvent;
+import pointGroups.gui.event.types.ShowNextEvent;
 import pointGroups.gui.event.types.ShowPreviousEvent;
+import pointGroups.gui.event.types.TutorialEvent;
 
 
 public class Menubar
@@ -27,8 +26,6 @@ public class Menubar
    * 
    */
   private static final long serialVersionUID = -6870787436220684514L;
-
-  private final EventDispatcher dispatcher;
 
   public final JMenu historyMenu;
   public final JMenu viewMenu;
@@ -51,7 +48,7 @@ public class Menubar
 
   public Menubar(final EventDispatcher dispatcher) {
     super();
-    this.dispatcher = dispatcher;
+
     historyMenu = new JMenu("History");
     viewMenu = new JMenu("View");
     debugMenu = new JMenu("Debug");
@@ -67,47 +64,56 @@ public class Menubar
     coordinateItem.setState(true);
 
     previousItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
+      @Override
+      public void actionPerformed(final ActionEvent event) {
         dispatcher.fireEvent(new ShowPreviousEvent());
       }
     });
 
     nextItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
+      @Override
+      public void actionPerformed(final ActionEvent event) {
         dispatcher.fireEvent(new ShowNextEvent());
       }
     });
-    
+
     showLogItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
+      @Override
+      public void actionPerformed(final ActionEvent event) {
         dispatcher.fireEvent(new ShowLogEvent());
       }
     });
-    
+
     tutorialItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
+      @Override
+      public void actionPerformed(final ActionEvent event) {
         dispatcher.fireEvent(new TutorialEvent());
       }
     });
 
     pointPickerItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
+      @Override
+      public void actionPerformed(final ActionEvent event) {
         dispatcher.fireEvent(new ShowFundamentalDomainEvent(
             pointPickerItem.getState()));
       }
     });
-    
+
     coordinateItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
-        dispatcher.fireEvent(new ShowCoordinateEvent(
-            coordinateItem.getState()));
+      @Override
+      public void actionPerformed(final ActionEvent event) {
+        dispatcher.fireEvent(new ShowCoordinateEvent(coordinateItem.getState()));
       }
     });
-    
-    previousItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
-    nextItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-    pointPickerItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.CTRL_MASK));
-    coordinateItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+
+    previousItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P,
+        ActionEvent.CTRL_MASK));
+    nextItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+        ActionEvent.CTRL_MASK));
+    pointPickerItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
+        ActionEvent.CTRL_MASK));
+    coordinateItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+        ActionEvent.CTRL_MASK));
     showLogItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F12, 0));
     tutorialItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
 
@@ -117,14 +123,13 @@ public class Menubar
     viewMenu.add(pointPickerItem);
     viewMenu.add(coordinateItem);
 
-    debugMenu.add(showLogItem); 
+    debugMenu.add(showLogItem);
 
     helpMenu.add(tutorialItem);
 
     this.add(historyMenu);
     this.add(viewMenu);
-    this.add(debugMenu);
+    // this.add(debugMenu);
     this.add(helpMenu);
   }
-
 }
