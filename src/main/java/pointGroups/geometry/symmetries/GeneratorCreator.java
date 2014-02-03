a9eed5dd17b1a5daf597705b05cb6a09c264c154
Oliver Wiese <oliver.wiese@fu-berlin.de>
2014-02-03 19:01:08 +0100

    update 4DRotation. Still to test

--- src/main/java/pointGroups/geometry/symmetries/GeneratorCreator.java
+++ src/main/java/pointGroups/geometry/symmetries/GeneratorCreator.java
@@ -29,8 +29,33 @@ public class GeneratorCreator
     List<Quaternion> gen = new ArrayList<Quaternion>();
     gen.add(qI);
     gen.add(qw);
-    return generateSymmetryGroup2(gen);
+    return generateSymmetryGroup3D(gen);
   }  
+  
+  
+  
+  
+  public static List<Rotation4D> TxTSymGroup(){
+    List<Rotation4D> gen = new ArrayList<Rotation4D>();
+    gen.add(new Rotation4D(Quaternion.I, Quaternion.ONE));
+    gen.add(new Rotation4D(qw, Quaternion.ONE));
+    gen.add(new Rotation4D(Quaternion.ONE,Quaternion.I));
+    gen.add(new Rotation4D(Quaternion.ONE, qw));
+    return generateSymmetryGroup4D(gen);
+
+
+    
+  }
+ 
+  public static List<Rotation4D> IcosahedralXIcosahedralSymmetryGroup(){
+     List<Rotation4D> gen = new ArrayList<Rotation4D>();
+     gen.add(new Rotation4D(qI, Quaternion.ONE));
+     gen.add(new Rotation4D(qw, Quaternion.ONE));
+     
+     gen.add(new Rotation4D(Quaternion.ONE,qI));
+     gen.add(new Rotation4D(Quaternion.ONE,qw));
+     return generateSymmetryGroup4D(gen);
+   }  
   
   /**
    * 
@@ -40,7 +65,7 @@ public class GeneratorCreator
       List<Quaternion> gen = new ArrayList<Quaternion>();
       gen.add(qO);
       gen.add(qI);
-      return generateSymmetryGroup(gen);
+      return generateSymmetryGroup3D(gen);
     } 
     
     
@@ -53,7 +78,7 @@ public class GeneratorCreator
         List<Quaternion> gen = new ArrayList<Quaternion>();
         gen.add(qT);
         gen.add(qw);
-        return generateSymmetryGroup(gen);
+        return generateSymmetryGroup3D(gen);
       }  
   
 
@@ -62,7 +87,7 @@ public class GeneratorCreator
    * @param generators
    * @return Groupelem generate by generators
    */
-  public static List<Quaternion> generateSymmetryGroup(List<Quaternion> generators) {
+  public static List<Quaternion> generateSymmetryGroup3D(List<Quaternion> generators) {
     int newElems = 0;
     List<Quaternion> group = new ArrayList<>(generators);
     List<Quaternion> newGroupelem = new ArrayList<>();
@@ -88,36 +113,36 @@ public class GeneratorCreator
   }
   
   
-  /**
-   * 
-   * @param generators
-   * @return Groupelem generate by generators
-   */
-  public static List<Quaternion> generateSymmetryGroup2(List<Quaternion> generators) {
+  public static List<Rotation4D> generateSymmetryGroup4D(List<Rotation4D> generators){
     int newElems = 0;
-    List<Quaternion> group = new ArrayList<>(generators);
-    List<Quaternion> newGroupelem = new ArrayList<>();
-    Quaternion z;
+    List<Rotation4D> group = new ArrayList<>(generators);
+    List<Rotation4D> newGroupelem = new ArrayList<>();
+    Rotation4D z;
+    int n = 0;
     do {
       newElems = 0;
-      for (Quaternion x : group) {
-        for(Quaternion g : generators){
-          z = x.mult(g);
-          z = g.inverse().mult(z);
+      for (Rotation4D x : group) {
+        for (Rotation4D y : group) {
+          z = x.nextRotation(y);
           if (!containsApprox(group, z) && !containsApprox(newGroupelem, z)) {
             newGroupelem.add(z);
             newElems++;
+            n++;
+            if(n%100 == 0){
+              System.out.println("n = "+n);
+            }
           }
         }
       }
-      for (Quaternion g : newGroupelem) {
+      for (Rotation4D g : newGroupelem) {
         group.add(g);
       }
       newGroupelem.clear();
     }
     while (newElems != 0);
-    return group;
+    return group;  
   }
+  
  
 
   private static boolean containsApprox(List<Quaternion> list, Quaternion x) {
@@ -129,6 +154,18 @@ public class GeneratorCreator
 
   private static boolean equalApprox(Quaternion a, Quaternion b) {
     double distance = Quaternion.distance(a, b);
+    return distance < epsilon;
+  } 
+  
+  private static boolean containsApprox(List<Rotation4D> list, Rotation4D x) {
+    for (Rotation4D y : list) {
+      if (equalApprox(x, y)) { return true; }
+    }
+    return false;
+  }
+
+  private static boolean equalApprox(Rotation4D a, Rotation4D b) {
+    double distance = a.distance(b);
     return distance < epsilon;
   } 
   
@@ -164,8 +201,8 @@ public class GeneratorCreator
       icosa.add(uq);
     }
     System.out.println("#Elemente bei Alex: "+icosa.size()+" #Elemente aus der Erzeuger: "+IcosahedralSymmetryGroup().size());
-    System.out.println(notInG2(icosa, IcosahedralSymmetryGroup())+" Elemnte aus der Alexgruppe sind nicht in der Gruppe mittels Erzeuger");
-    System.out.println(notInG2(IcosahedralSymmetryGroup(),icosa)+" Elemnte aus der Erzeugergruppe snd nicht in der Gruppe von Alex");
+    
+    System.out.println("#Elem IxI: "+IcosahedralXIcosahedralSymmetryGroup().size());
   }
   
  
