c5db4e987ff0808d926c1cbfeff95aa339fc6949
Simon Tippenhauer <simon.tippenhauer@fu-berlin.de>
2014-02-05 21:15:13 +0100

    Logger added, logging for changed slider values

--- src/main/java/pointGroups/gui/ScalingSlider.java
+++ src/main/java/pointGroups/gui/ScalingSlider.java
@@ -1,11 +1,14 @@
 package pointGroups.gui;
 
+import java.util.logging.Logger;
+
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import pointGroups.gui.event.EventDispatcher;
 import pointGroups.gui.event.types.ScaleFundamentalDomainEvent;
+import pointGroups.util.LoggerFactory;
 
 public class ScalingSlider
   extends JSlider
@@ -17,11 +20,12 @@ public class ScalingSlider
   protected final static int INIT_VALUE = 1;
   
   protected EventDispatcher dispatcher = EventDispatcher.get();
+  private final Logger logger = LoggerFactory.get(this.getClass());
   
   public ScalingSlider() {
     super(JSlider.HORIZONTAL, MIN_VALUE, MAX_VALUE, INIT_VALUE);
     
-    this.setMajorTickSpacing(9);
+    this.setMajorTickSpacing(3);
     this.setMinorTickSpacing(1);
     this.setPaintTicks(true);
     this.setPaintLabels(true);
@@ -32,9 +36,11 @@ public class ScalingSlider
   
   class SliderListener implements ChangeListener {
     public void stateChanged(ChangeEvent e) {
-        JSlider source = (JSlider) e.getSource();
-        if (!source.getValueIsAdjusting()) {
-            dispatcher.fireEvent(new ScaleFundamentalDomainEvent(source.getValue()));
+      JSlider source = (JSlider) e.getSource();
+      if (!source.getValueIsAdjusting()) {
+        int value = source.getValue();
+        dispatcher.fireEvent(new ScaleFundamentalDomainEvent(value));
+        logger.info("ScalingSlider: Value set to " + value);
         }    
     }
   }
