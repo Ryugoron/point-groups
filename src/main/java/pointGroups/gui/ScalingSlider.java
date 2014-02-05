5f0815b5a65b83000f608a3dfcf33e4adb6651a4
Simon Tippenhauer <simon.tippenhauer@fu-berlin.de>
2014-02-05 21:05:45 +0100

    adding change listener for slider. Fires ScaleFundamentalDomainEvent on change.

--- src/main/java/pointGroups/gui/ScalingSlider.java
+++ src/main/java/pointGroups/gui/ScalingSlider.java
@@ -1,6 +1,11 @@
 package pointGroups.gui;
 
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import pointGroups.gui.event.EventDispatcher;
+import pointGroups.gui.event.types.ScaleFundamentalDomainEvent;
 
 public class ScalingSlider
   extends JSlider
@@ -11,6 +16,8 @@ public class ScalingSlider
   protected final static int MAX_VALUE = 10;
   protected final static int INIT_VALUE = 1;
   
+  protected EventDispatcher dispatcher = EventDispatcher.get();
+  
   public ScalingSlider() {
     super(JSlider.HORIZONTAL, MIN_VALUE, MAX_VALUE, INIT_VALUE);
     
@@ -19,6 +26,17 @@ public class ScalingSlider
     this.setPaintTicks(true);
     this.setPaintLabels(true);
     this.setSnapToTicks(true);
+    
+    this.addChangeListener(new SliderListener());
+  }
+  
+  class SliderListener implements ChangeListener {
+    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider) e.getSource();
+        if (!source.getValueIsAdjusting()) {
+            dispatcher.fireEvent(new ScaleFundamentalDomainEvent(source.getValue()));
+        }    
+    }
   }
 
 }
