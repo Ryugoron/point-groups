e99f837ecf45a735d78639e717ad9ac9cb5fd105
Simon Tippenhauer <simon.tippenhauer@fu-berlin.de>
2014-04-11 09:07:16 +0200

    overlooked already existing event for tutorial. adjustment of tutorial frame.

--- src/main/java/pointGroups/gui/TutorialFrame.java
+++ src/main/java/pointGroups/gui/TutorialFrame.java
@@ -2,12 +2,13 @@ package pointGroups.gui;
 
 import javax.swing.JFrame;
 
-import pointGroups.gui.event.types.ShowTutorialEvent;
-import pointGroups.gui.event.types.ShowTutorialHandler;
+import pointGroups.gui.event.EventDispatcher;
+import pointGroups.gui.event.types.TutorialEvent;
+import pointGroups.gui.event.types.TutorialHandler;
 
 public class TutorialFrame 
   extends JFrame
-  implements ShowTutorialHandler
+  implements TutorialHandler
 {
   protected static int defaultWidth = 600;
   protected static int defaultHeight = 400;
@@ -23,12 +24,16 @@ public class TutorialFrame
   public TutorialFrame(int width, int height) {
     super("Tutorial");
     this.setSize(width, height);
+    
+    this.setDefaultCloseOperation(HIDE_ON_CLOSE);
+    EventDispatcher.get().addHandler(TutorialEvent.TYPE, this);
+    this.setVisible(true);
   }
 
   @Override
-  public void onShowTutorialEvent(ShowTutorialEvent event) {
-    // TODO Auto-generated method stub
-    
+  public void onTutorialEvent(TutorialEvent event) {
+    this.setVisible(true);
+    this.toFront();
   }
 
 }
