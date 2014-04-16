b0c6e38244b4bef485482b5fe3a353e260db438d
Alex <a.steen@fu-berlin.de>
2014-04-16 11:57:18 +0200

    added more math text and added 4d symmetries

--- src/main/java/pointGroups/gui/symchooser/SymmetryPanel.java
+++ src/main/java/pointGroups/gui/symchooser/SymmetryPanel.java
@@ -15,6 +15,7 @@ import pointGroups.geometry.Point3D;
 import pointGroups.geometry.Point4D;
 import pointGroups.geometry.Symmetry;
 import pointGroups.geometry.symmetries.Symmetry3D;
+import pointGroups.geometry.symmetries.Symmetry4D;
 import pointGroups.gui.event.EventDispatcher;
 import pointGroups.gui.event.types.DimensionSwitchEvent;
 import pointGroups.gui.symchooser.elements.SymmetryList;
@@ -89,8 +90,8 @@ public class SymmetryPanel
       this.symmetries3D.addElement(sym);
     }
     // 4D Symmetries
-    // for (Symmetry4D sym : Symmetry4D.getSymmetries()) {
-    // this.symmetries4D.addElement(sym);
-    // }
+    for (Symmetry4D sym : Symmetry4D.getSymmetries()) {
+      this.symmetries4D.addElement(sym);
+    }
   }
 }
