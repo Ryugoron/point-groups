2da094cb9c5b54bca4d42839a67843444da5ba30
Nadja <scharfn@zedat.fu-berlin.de>
2014-05-03 15:41:30 +0200

    If settings.ini doesnt exist, a new one is generated

--- src/main/java/pointGroups/PointGroups.java
+++ src/main/java/pointGroups/PointGroups.java
@@ -2,8 +2,15 @@ package pointGroups;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -21,6 +28,7 @@ import pointGroups.gui.event.types.Schlegel3DComputeEvent;
 import pointGroups.gui.event.types.Symmetry3DChooseEvent;
 import pointGroups.util.ExternalCalculationWrapper;
 import pointGroups.util.PointGroupsUtility;
+import pointGroups.util.PointerToString;
 import pointGroups.util.polymake.wrapper.PolymakeWrapper;
 
 
@@ -31,9 +39,44 @@ public class PointGroups
 
   public static void main(final String[] args)
     throws IOException {
-
-    File polyCmd = PointGroupsUtility.getPolymakePath();
-    File driverPath = PointGroupsUtility.getPolymakeDriverPath();
+    File polyCmd;
+    File driverPath;
+    try{
+    polyCmd = PointGroupsUtility.getPolymakePath();
+    driverPath = PointGroupsUtility.getPolymakeDriverPath();
+    }
+    catch (FileNotFoundException e) {
+      PointerToString ps = new PointerToString();
+      new StartupErrorFrame(ps);
+      synchronized (ps) {
+        try {
+          ps.wait();
+        }
+        catch (InterruptedException e1) {
+          // TODO Auto-generated catch block
+          e1.printStackTrace();
+        }
+      }
+      System.out.println("Entered Path is:" + ps.s);
+      //create two files because while running the first time, the one in target doesnt exist.
+      File file = new File("./src/main/resources/settings.ini");
+      File file2 = new File("./target/classes/settings.ini");
+      try {
+        System.out.println(file.createNewFile());
+        BufferedWriter output = new BufferedWriter(new FileWriter(file));
+        output.write("POLYMAKEPATH = " + ps.s);
+        output.close();
+        output = new BufferedWriter(new FileWriter(file2));
+        output.write("POLYMAKEPATH = " + ps.s);
+        output.close();
+      } 
+      catch (IOException e2) {
+        System.err.println("settings.ini not proper initialised");
+         e.printStackTrace();
+      }
+      polyCmd = PointGroupsUtility.getPolymakePath();
+      driverPath = null;
+    }
 
     // Check whether Polymake exists on the system
     if (!polyCmd.isFile()) {
